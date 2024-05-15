a70fdf2aa495a5c60785cfc7473609f7a2939763
Divo123 <topherharsh@gmail.com>
2024-05-15 16:33:12 +0800

    fixed typing in navbar

--- src/scenes/classes/Class.tsx
+++ src/scenes/classes/Class.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export type ClassType = {
   name: string;
   description?: string;
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
-import Link from "@/scenes/navbar/Link";
+import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/UseMediaQuery";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
 import ActionButton from "@/shared/ActionButton";
 
 type Props = {
@@ -12,27 +12,25 @@ type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Navbar = ({ selectedPage, setSelectedPage, isTopOfPage }: Props) => {
-  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
-  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
   const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
+
   return (
     <nav>
-      {/* big navbar */}
       <div
         className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
       >
-        {/* inner navbar that contains all the routes */}
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
-            {/* Left Side  (logo)*/}
+            {/* LEFT SIDE */}
             <img alt="logo" src={Logo} />
 
-            {/* Right Side */}
+            {/* RIGHT SIDE */}
             {isAboveMediumScreens ? (
               <div className={`${flexBetween} w-full`}>
-                {/* all the routes */}
                 <div className={`${flexBetween} gap-8 text-sm`}>
                   <Link
                     page="Home"
@@ -55,11 +53,10 @@ const Navbar = ({ selectedPage, setSelectedPage, isTopOfPage }: Props) => {
                     setSelectedPage={setSelectedPage}
                   />
                 </div>
-                {/* sign in and become a member */}
                 <div className={`${flexBetween} gap-8`}>
-                  <p>Sign in</p>
-                  <ActionButton setSelectedPage={selectedPage}>
-                    Become a member
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
                   </ActionButton>
                 </div>
               </div>
@@ -84,8 +81,9 @@ const Navbar = ({ selectedPage, setSelectedPage, isTopOfPage }: Props) => {
               <XMarkIcon className="h-6 w-6 text-gray-400" />
             </button>
           </div>
-          {/* all the routes */}
-          <div className={`ml-[33%] flex flex-col gap-10 text-2xl`}>
+
+          {/* MENU ITEMS */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
             <Link
               page="Home"
               selectedPage={selectedPage}
@@ -107,7 +105,6 @@ const Navbar = ({ selectedPage, setSelectedPage, isTopOfPage }: Props) => {
               setSelectedPage={setSelectedPage}
             />
           </div>
-          {/* sign in and become a member */}
         </div>
       )}
     </nav>
